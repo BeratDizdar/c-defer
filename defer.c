8dc6fd14e31dc5114f7650400d8da7fd1ea199b3
BeratDizdar <139409706+BeratDizdar@users.noreply.github.com>
2026-07-09 10:42:15 +0300

    çalışıyor işte

---
+++ defer.c
@@ -0,0 +1,15 @@
+/* simple as that */
+void(*fn_list[999])(void);t;
+defer(void(*f)(void)){if(t>=999)return-1;fn_list[t++]=f;}
+exec_defer(){for(;t>0;)fn_list[--t]();}
+
+/* example */
+#include <stdio.h>
+void prt(){printf("msg1\n");}
+main() {
+    defer(prt);
+    printf("msg2\n");
+    exec_defer();
+    printf("msg3\n");
+    return 0;
+}
